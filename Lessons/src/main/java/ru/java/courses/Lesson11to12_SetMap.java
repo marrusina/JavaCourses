372222ceb8fc67d9aeed5a983e75b6413b05bd6c
marina rusina <rusina@reksoft.ru>
2019-04-24 22:46:02 +0300

    11-12 setmap

--- Lessons/src/main/java/ru/java/courses/Lesson11to12_SetMap.java
+++ Lessons/src/main/java/ru/java/courses/Lesson11to12_SetMap.java
@@ -1,29 +1,22 @@
 package ru.java.courses;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 import javax.swing.tree.TreeCellEditor;
 
 public class Lesson11to12_SetMap {
 
-    public static class User {
+    public static class User implements Comparable<User>{
 
         private String name;
-        private int age;
+        private Integer age;
         private String phone;
 
-        public User(String name, int age) {
+        public User(String name, Integer age) {
             this.name = name;
             this.age = age;
         }
@@ -40,11 +33,11 @@ public class Lesson11to12_SetMap {
             this.name = name;
         }
 
-        public int getAge() {
+        public Integer getAge() {
             return age;
         }
 
-        public void setAge(int age) {
+        public void setAge(Integer age) {
             this.age = age;
         }
 
@@ -54,6 +47,30 @@ public class Lesson11to12_SetMap {
 
         public void setPhone(String phone) {
             this.phone = phone;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            User user = (User) o;
+            return age == user.age &&
+                    Objects.equals(name, user.name) &&
+                    Objects.equals(phone, user.phone);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, age, phone);
+        }
+
+        @Override
+         public int compareTo(User o) {
+           // int com = o1.name.compareTo(o2.name);
+            if (name.compareTo(o.name) == 0) {
+                return age.compareTo(o.age);
+            }
+            return name.compareTo(o.name);
         }
     }
 
@@ -65,10 +82,14 @@ public class Lesson11to12_SetMap {
      * 4. Возвращаем последнего пользователя
      */
     public static User task1(Collection<User> source) {
-        // свой код нужно писать тут
-        // следующую строку можно удалять
 
-        return null;
+
+         TreeSet<User> test = new TreeSet<>(source);
+       //Stream<User> test2 = source.stream().filter(Objects::nonNull).distinct().skip(source.size() - 1);
+
+       //TreeSet<User> test3 = new TreeSet<User>((Collection<? extends User>) test);
+       //return test.collect(Collectors.toSet());
+        return test.last();
     }
 
     /**
@@ -79,16 +100,14 @@ public class Lesson11to12_SetMap {
      * 4. Вернуть количество записей в справочнике
      */
     public static int task2(Collection<User> source) {
-        // свой код нужно писать тут
-        // следующую строку можно удалять
 
-       return 0;
+        return source.stream().collect(Collectors.toSet()).size();
+
     }
-
 
     /**
      * В этом методе необходимо реализовать следующий алгоритм:
-     * 1. На вход получаем список названий книг
+     * 1. На вход получаем список названий книг
      * 2. Распределяем книги по полкам так, чтобы на каждой полке было примерно одинаковое количество книг
      * 3. Все книги должны быть отсортированы по алфавиту с первой до последней полки
      * 4. Количество полок константное - 5 штук
@@ -97,21 +116,32 @@ public class Lesson11to12_SetMap {
      * Нумерация полок начинается с нуля!
      */
     public static Map task3(Collection<String> source) {
-        // свой код нужно писать тут
-        // следующую строку можно удалять
 
-        return null;
+        HashMap<Integer, List<String>> test = new HashMap<>();
+        List<String> strings = new ArrayList<>(source);
+        int i = 0;
+        IntStream.range(0,5).forEach((k) -> {test.put(i, strings.subList(i, strings.size() /5));});
+
+       // source.stream().collect(HashMap::new, (h,o) -> h.put(h.size(), o), (h,o) -> {}).forEach(i,o) -> {strings.add(i,source);};
+
+      // Stream.of(source).filter( n -> n.size() <= 1).collect( Collectors.toList());
+//        //int i = 0;
+        //strings.stream().range(0,source.size()).filter(i -> i%5 == 0).mapToObj(i -> source).collect(Collectors.toList());
+//        return test;
+        return test;
     }
+
 
 
     /**
      * В этом методе необходимо реализовать следующий алгоритм:
-     * 1. На вход получаем книги, распределенные по полкам
+     * 1. На вход получаем книги, распределенные по полкам
      * 5. Вернуть справочник [название книги -> номер полки]
      */
     public static Map task4(Map<Integer, String> source) {
-        // свой код нужно писать тут
-        // следующую строку можно удалять
-        return null;
+
+        Map<String, Integer> test = new HashMap<>();
+                source.entrySet().forEach(i -> {test.put(i.getValue(),i.getKey());});
+                return test;
     }
 }
